f1d943340d9a6420fbdbe7d5a1badfb17be22318
PreethiBompally <pb54108@uga.edu>
2024-09-30 23:20:58 -0400

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,8 @@
 mod dns;
 mod tcp;
-mod tls;
-mod file_utils;
-mod downloader;
 
 use std::io::{self, Read, Write};
 use clap::Parser;
-use std::thread;
-use std::fs::OpenOptions;
-use std::fs::File;
 
 #[derive(Parser, Debug)]
 #[clap(author, version, about, long_about = None)]
@@ -30,14 +24,15 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     let path = format!("/{}", path);
 
     let ip_address = dns::get_request_ip(host_name)?;
-    let tcp_stream = tcp::establish_tcp_socket(&ip_address, 443)?;
+    let mut tcp_stream = tcp::establish_tcp_socket(&ip_address, 443)?;
     
     let head_request = format!(
         "HEAD {} HTTP/1.1\r\nHost: {}\r\nConnection: close\r\n\r\n",
         path, host_name
     );
-    tls_stream.write_all(head_request.as_bytes())?;
+    tcp_stream.write_all(head_request.as_bytes())?;
 
     println!("Successfully established a TCP connection to {}", host_name);
     println!("Basic GET request sent to the server.");
+    Ok(())
 }
