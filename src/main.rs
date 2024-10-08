b31f10712ce0988345d892de5667420d5c2ea9d5
PreethiBompally <pb54108@uga.edu>
2024-10-08 13:02:50 -0400

    Rectified parsing error

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 mod dns;
 mod tcp;
 
-use std::io::{self, Read, Write};
+use std::io::{Write};
 use clap::Parser;
 
 #[derive(Parser, Debug)]
@@ -20,12 +20,20 @@ struct Args {
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     let args = Args::parse();
 
-    let (host_name, path) = args.url.split_once('/').ok_or("Invalid URL")?;
-    let path = format!("/{}", path);
+    // Correcting URL parsing
+    let url_parts: Vec<&str> = args.url.split("://").collect();
+    let domain_with_path = url_parts.last().unwrap_or(&"");
+    let domain_parts: Vec<&str> = domain_with_path.split('/').collect();
+    let host_name = domain_parts[0];
+    let path = if domain_parts.len() > 1 {
+        format!("/{}", domain_parts[1..].join("/"))
+    } else {
+        "/".to_string() // Assume root if no specific path is provided
+    };
 
     let ip_address = dns::get_request_ip(host_name)?;
     let mut tcp_stream = tcp::establish_tcp_socket(&ip_address, 443)?;
-    
+
     let head_request = format!(
         "HEAD {} HTTP/1.1\r\nHost: {}\r\nConnection: close\r\n\r\n",
         path, host_name
