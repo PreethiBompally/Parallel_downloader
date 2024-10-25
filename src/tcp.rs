0d6415c9386e72e8e53a4bd6e23b1569673bdeee
saimukeshch <saimukesh.ch@gmail.com>
2024-10-24 22:35:19 -0400

    Update tcp.rs

--- src/tcp.rs
+++ src/tcp.rs
@@ -1,5 +1,8 @@
-use std::net::TcpStream;
-
-pub fn establish_tcp_socket(ip_address: &str, port: u16) -> Result<TcpStream, std::io::Error> {
-    TcpStream::connect((ip_address, port))
+use crate::error::DownloaderError;
+use std::net::{TcpStream, IpAddr, SocketAddr};
+ 
+pub fn establish_tcp_socket(ip_address: IpAddr, port: u16) -> Result<TcpStream, DownloaderError> {
+    let socket_addr = SocketAddr::new(ip_address, port);
+    TcpStream::connect(socket_addr)
+        .map_err(|e| DownloaderError::ConnectionError(e.to_string()))
 }
